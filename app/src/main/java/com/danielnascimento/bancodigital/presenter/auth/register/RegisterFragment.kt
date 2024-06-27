e681d861d2a652a32f5253ff05979b99db870cc7
Daniel Nascimento <pdaniel.contato@gmail.com>
2024-06-26 23:45:36 -0300

    style: register screen

--- app/src/main/java/com/danielnascimento/bancodigital/presenter/auth/register/RegisterFragment.kt
+++ app/src/main/java/com/danielnascimento/bancodigital/presenter/auth/register/RegisterFragment.kt
@@ -1,20 +1,20 @@
-package com.danielnascimento.bancodigital.presenter.auth.recover
+package com.danielnascimento.bancodigital.presenter.auth.register
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.danielnascimento.bancodigital.databinding.FragmentRecoverAccountBinding
+import com.danielnascimento.bancodigital.databinding.FragmentRegisterBinding
 
-class RecoverAccountFragment : Fragment() {
-    private var _binding: FragmentRecoverAccountBinding? = null
+class RegisterFragment : Fragment() {
+    private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        _binding = FragmentRecoverAccountBinding.inflate(inflater, container, false)
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
         return binding.root
     }
 
