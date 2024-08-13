24c40d27bf00758deb6c3c04e1ff450ec20416ec
Daniel Nascimento <pdaniel.contato@gmail.com>
2024-08-13 19:28:35 -0300

    feat: register implementation

--- app/src/main/java/com/danielnascimento/bancodigital/data/repository/auth/AuthFirebaseDataSource.kt
+++ app/src/main/java/com/danielnascimento/bancodigital/data/repository/auth/AuthFirebaseDataSource.kt
@@ -1,7 +1,9 @@
 package com.danielnascimento.bancodigital.data.repository.auth
 
+import com.google.firebase.auth.FirebaseUser
+
 interface AuthFirebaseDataSource {
     suspend fun login(email: String, password: String)
-    suspend fun register(name: String, email: String, phone: String, password: String)
+    suspend fun register(name: String, email: String, phone: String, password: String): FirebaseUser
     suspend fun recover(email: String)
 }
--- app/src/main/java/com/danielnascimento/bancodigital/data/repository/auth/AuthFirebaseDataSourceImpl.kt
+++ app/src/main/java/com/danielnascimento/bancodigital/data/repository/auth/AuthFirebaseDataSourceImpl.kt
@@ -1,16 +1,40 @@
 package com.danielnascimento.bancodigital.data.repository.auth
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.FirebaseDatabase
+import kotlin.coroutines.suspendCoroutine
 
 class AuthFirebaseDataSourceImpl(
-    firebaseDatabase: FirebaseDatabase
+    private val firebaseDatabase: FirebaseDatabase,
+    private val firebaseAuth: FirebaseAuth
 ) : AuthFirebaseDataSource {
     override suspend fun login(email: String, password: String) {
         TODO("Not yet implemented")
     }
 
-    override suspend fun register(name: String, email: String, phone: String, password: String) {
-        TODO("Not yet implemented")
+    override suspend fun register(
+        name: String,
+        email: String,
+        phone: String,
+        password: String
+    ): FirebaseUser {
+        return suspendCoroutine { continuation ->
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val user = task.result.user
+
+                        user?.let {
+                            continuation.resumeWith(Result.success(user))
+                        }
+                    } else {
+                        task.exception?.let {
+                            continuation.resumeWith(Result.failure(it))
+                        }
+                    }
+                }
+        }
     }
 
     override suspend fun recover(email: String) {
